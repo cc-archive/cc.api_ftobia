425b5c7bc9c6365dad289dbc5f940e20e9c87a20
Frank Tobia <frank.tobia@gmail.com>
2008-07-03 16:42:42 -0700

    Add dependencies for lxml and coverage packages.

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
                       'setuptools',
                       'zope.interface',
                       'nose',
+                      'lxml',
+                      'coverage',
                      ],
     packages=find_packages(exclude=['ez_setup','examples','tests']),
     include_package_data=True,

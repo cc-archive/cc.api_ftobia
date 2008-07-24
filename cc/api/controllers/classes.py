339a5f1a76186dbbeee120a25c93852bdc44c25f
Frank Tobia <frank.tobia@gmail.com>
2008-07-24 14:10:42 -0700

    Implement locale option for /classes (even though backend doesn't support it).

--- cc/api/controllers/classes.py
+++ cc/api/controllers/classes.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from cc.api.lib.base import BaseController
+from cc.api.lib.base import request, BaseController
 import cc.license
 
 log = logging.getLogger(__name__)
@@ -14,7 +14,14 @@ class ClassesController(BaseController):
         for lname in cc.license.selectors.list():
             lclasses.append(cc.license.selectors.choose(lname))
 
+        # get locale option
+        try:
+            locale = request.GET['locale']
+        except KeyError:
+            locale = 'en'
+
         # display them
         tmpl = self.loader.load('classes.xml')
-        stream = tmpl.generate(lclasses=lclasses)
+        stream = tmpl.generate(lclasses=lclasses,
+                               locale=locale)
         return stream.render(method='xml')

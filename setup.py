7e13ea56f1478a8908ce7fabbf3e19b22f692706
Frank Tobia <frank.tobia@gmail.com>
2008-07-29 16:57:15 -0700

    Remove symlink hack, add cc.license as a dependency. Uploaded cc.license to PyPI so it all works :)

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
                       'lxml',
                       'coverage',
                       'Genshi',
+                      'cc.license',
                      ],
     packages=find_packages(exclude=['ez_setup','examples','tests']),
     include_package_data=True,
